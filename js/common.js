bd5124bada76ee06aabf2d278c778376c9553828
oDaviano <pak961113@naver.com>
2024-12-23 17:42:08 +0900

    Add image files Add naver map api Add sidebar layout Add search component Edit common.js Edit commont.css Edit main page css

--- js/common.js
+++ js/common.js
@@ -1,45 +1,58 @@
 let _device = {};
 
 const publish = (function () {
-	const common = {
-		init: function () {
-			common.setCommon();
-		},
+  const common = {
+    init: function () {
+      common.setCommon();
+    },
 
-	
-		setCommon: function () {
-			let currentPage = window.location.pathname; // 현재 페이지 경로
-			let basePath = currentPage.includes("index.html") ? "pages/" : "../pages/"; // 경로 설정
-			let url = basePath + "common.html"; // 최종 경로
-			let selector = [".header-pc1", ".header-pc2", ".footer-pc"];
-			let identifier = "load";
-			$.ajax({
-				type: "GET",
-				url: url,
-				dataType: "html",
-				// async: false,
-				cache: false,
-				success: function (html, textStatus) {
-					for (let i = 0; i < selector.length; i++) {
-						let take = $(html).find(selector[i]).html();
-						$(selector[i] + "." + identifier)
-							.html(take)
-							.removeClass(identifier);
-					}
-                    console.log("ajax loaded");
-				},
-				error: function (xhr, textStatus, errorThrown) {
-					if (window.console !== undefined) {
-						console.log(errorThrown ? errorThrown : xhr.status);
-					}
-					return false;
-				},
-			});
+    setCommon: function () {
+      let currentPage = window.location.pathname; // 현재 페이지 경로
+      let basePath = currentPage.includes("index.html")
+        ? "pages/"
+        : "../pages/"; // 경로 설정
+      let url = basePath + "common.html"; // 최종 경로
+      let selector = [".header-pc1", ".header-pc2", ".footer-pc"];
+      let identifier = "load";
+      $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "html",
+        // async: false,
+        cache: false,
+        success: function (html, textStatus) {
+          for (let i = 0; i < selector.length; i++) {
+            let take = $(html).find(selector[i]).html();
+            $(selector[i] + "." + identifier)
+              .html(take)
+              .removeClass(identifier);
+          }
+          common.setAside();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+          if (window.console !== undefined) {
+            console.log(errorThrown ? errorThrown : xhr.status);
+          }
+          return false;
+        },
+      });
+    },
+    setAside: function () {
+			// 모바일 전체메뉴 오픈
+				const $body = $("body");
+				const $aside = $("aside");
+				const $buttonMenu = $(".button-menu");
+				$buttonMenu.on("click", function () {
+					$body.toggleClass("ovy-hidden");
+          $aside.toggleClass("active");
+					// $aside.toggleClass("active", $(this).hasClass("open-menu"));
+				});
+			
 		},
-	};
-	return common;
+  };
+  return common;
 })();
 
 $(function () {
-	publish.init();
+  publish.init();
 });
